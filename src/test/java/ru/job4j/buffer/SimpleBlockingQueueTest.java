b03cd8d8b4adf9653daa834755b1cc66cb7aec7d
Vetoshkin-Oleg <voa2014@gmail.com>
2024-01-31 14:06:08 +0800

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/buffer/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/buffer/SimpleBlockingQueueTest.java
@@ -2,6 +2,8 @@ package ru.job4j.buffer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleBlockingQueueTest {
@@ -36,5 +38,41 @@ class SimpleBlockingQueueTest {
         producer.join();
         consumer.join();
         assertThat(simpleBlockingQueue.poll()).isEqualTo(100);
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(20);
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 5; i++) {
+                            queue.offer(i);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                }, "Producer"
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }, "Consumer"
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
     }
 }
