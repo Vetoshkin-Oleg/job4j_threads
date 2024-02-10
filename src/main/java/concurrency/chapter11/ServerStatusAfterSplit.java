f966d557ea2da8f175f9a66a1e19217f586045fa
Vetoshkin-Oleg <voa2014@gmail.com>
2024-02-11 00:35:23 +0800

    Java. Concurrency in Practice. Chapter 11

---
+++ src/main/java/concurrency/chapter11/ServerStatusAfterSplit.java
@@ -0,0 +1,49 @@
+package concurrency.chapter11;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ServerStatusAfterSplit
+ * <p/>
+ * ServerStatus refactored to use split locks
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class ServerStatusAfterSplit {
+    @GuardedBy("users") public final Set<String> users;
+    @GuardedBy("queries") public final Set<String> queries;
+
+    public ServerStatusAfterSplit() {
+        users = new HashSet<>();
+        queries = new HashSet<>();
+    }
+
+    public void addUser(String u) {
+        synchronized (users) {
+            users.add(u);
+        }
+    }
+
+    public void addQuery(String q) {
+        synchronized (queries) {
+            queries.add(q);
+        }
+    }
+
+    public void removeUser(String u) {
+        synchronized (users) {
+            users.remove(u);
+        }
+    }
+
+    public void removeQuery(String q) {
+        synchronized (users) {
+            queries.remove(q);
+        }
+    }
+}
