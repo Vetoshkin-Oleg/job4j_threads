d62dfd645b2226026c34bb7d5748c402cdd3edd4
Vetoshkin-Oleg <voa2014@gmail.com>
2024-02-25 23:38:49 +0800

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/notification/EmailNotification.java
@@ -0,0 +1,25 @@
+package ru.job4j.notification;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        String subject = "Notification " + user.username() + " to email " + user.email();
+        String body = "Add a new event to " + user.username();
+
+        pool.submit(() -> send(subject, body, user.email()));
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+}
