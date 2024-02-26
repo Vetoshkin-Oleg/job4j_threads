6993c1510706ecabbaf8566a649b23ad9ddf1b82
Vetoshkin-Oleg <voa2014@gmail.com>
2024-02-26 23:09:40 +0800

    1. Реализовать ThreadPool [#1099 #483171]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -7,16 +7,14 @@ import java.util.List;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks;
-    private final int size;
+    private SimpleBlockingQueue<Runnable> tasks;
+    private final int size = Runtime.getRuntime().availableProcessors();
 
     public ThreadPool() {
-        tasks = new SimpleBlockingQueue<>(10);
-        size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
             threads.add(new Thread(() -> {
                 try {
-                    tasks.poll();
+                    tasks.poll().run();
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
@@ -38,5 +36,34 @@ public class ThreadPool {
         for (Thread runnable : threads) {
             runnable.interrupt();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int limit = 10;
+        ThreadPool threadPool = new ThreadPool();
+        threadPool.tasks = new SimpleBlockingQueue<>(limit);
+        Runnable job0 = () -> System.out.println("Задача 0");
+        Runnable job1 = () -> System.out.println("Задача 1");
+        Runnable job2 = () -> System.out.println("Задача 2");
+        Runnable job3 = () -> System.out.println("Задача 3");
+        Runnable job4 = () -> System.out.println("Задача 4");
+        Runnable job5 = () -> System.out.println("Задача 5");
+        Runnable job6 = () -> System.out.println("Задача 6");
+        Runnable job7 = () -> System.out.println("Задача 7");
+        Runnable job8 = () -> System.out.println("Задача 8");
+        Runnable job9 = () -> System.out.println("Задача 9");
+
+        threadPool.work(job0);
+        threadPool.work(job1);
+        threadPool.work(job2);
+        threadPool.work(job3);
+        threadPool.work(job4);
+        threadPool.work(job5);
+        threadPool.work(job6);
+        threadPool.work(job7);
+        threadPool.work(job8);
+        threadPool.work(job9);
+
+        threadPool.initialize();
     }
 }
