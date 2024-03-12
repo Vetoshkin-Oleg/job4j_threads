f2a08838626663c64d4579a4043562b0e44d5ffc
Vetoshkin-Oleg <voa2014@gmail.com>
2024-03-12 23:33:03 +0800

    4. CompletableFuture [#361626 #485513]

--- src/main/java/ru/job4j/pools/RowColSum.java
+++ src/main/java/ru/job4j/pools/RowColSum.java
@@ -4,26 +4,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RowColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         Sums[] result = new Sums[matrix.length];
--- src/test/java/ru/job4j/pools/RowColSumTest.java
+++ src/test/java/ru/job4j/pools/RowColSumTest.java
@@ -18,7 +18,7 @@ class RowColSumTest {
                 {1, 2, 3, 4, 5, 6, 7},
                 {1, 2, 3, 4, 5, 6, 7},
                 {1, 2, 3, 4, 5, 6, 7}};
-        RowColSum.Sums[] sums = RowColSum.sum(matrix);
+        Sums[] sums = RowColSum.sum(matrix);
         int sumOf3 = sums[3].getColSum() + sums[3].getRowSum();
         assertThat(sumOf3).isEqualTo(56);
     }
@@ -34,7 +34,7 @@ class RowColSumTest {
                         {8, 7, 6, 5, 4, 3, 2, 1},
                         {8, 7, 6, 5, 4, 3, 2, 1},
                         {8, 7, 6, 5, 4, 3, 2, 1}};
-        RowColSum.Sums[] sums = RowColSum.sum(matrix);
+        Sums[] sums = RowColSum.sum(matrix);
         int sumOf7 = sums[7].getColSum() + sums[7].getRowSum();
         assertThat(sumOf7).isEqualTo(44);
     }
@@ -49,7 +49,7 @@ class RowColSumTest {
                         {1, 2, 3, 4, 5, 6, 7},
                         {1, 2, 3, 4, 5, 6, 7},
                         {1, 2, 3, 4, 5, 6, 7}};
-        RowColSum.Sums[] sums = RowColSum.asyncSum(matrix);
+        Sums[] sums = RowColSum.asyncSum(matrix);
         int sumOf3 = sums[3].getColSum() + sums[3].getRowSum();
         assertThat(sumOf3).isEqualTo(56);
     }
@@ -65,8 +65,27 @@ class RowColSumTest {
                         {8, 7, 6, 5, 4, 3, 2, 1},
                         {8, 7, 6, 5, 4, 3, 2, 1},
                         {8, 7, 6, 5, 4, 3, 2, 1}};
-        RowColSum.Sums[] sums = RowColSum.asyncSum(matrix);
+        Sums[] sums = RowColSum.asyncSum(matrix);
         int sumOf7 = sums[7].getColSum() + sums[7].getRowSum();
         assertThat(sumOf7).isEqualTo(44);
+    }
+
+    @Test
+    void asyncSumArrayComparison() throws ExecutionException, InterruptedException {
+        int[][] matrix =
+                        {{8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1},
+                        {8, 7, 6, 5, 4, 3, 2, 1}};
+        Sums[] sums = RowColSum.asyncSum(matrix);
+        Sums[] sumsExpected = new Sums[1];
+        sumsExpected[0] = new Sums();
+        sumsExpected[0].setRowSum(36);
+        sumsExpected[0].setColSum(8);
+        assertThat(sumsExpected[0]).isEqualTo(sums[7]);
     }
 }
